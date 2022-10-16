9954e9ca38f7aabd571b557c3c5cfb928dc2cfd7
Matheus Martinez <matheus.martinez@ctw.bmwgroup.com>
2022-10-16 18:10:03 +0100

    Added first Button Componnent Story

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,39 @@
 import { styled } from './styles'
+import { ComponentProps } from 'react'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$green500',
   borderRadius: '$md',
-  padding: '$4',
+  padding: '$3 $4',
+  border: 0,
+  color: '$white',
+  fontWeight: '$bold',
+
+  '&:hover': {
+    backgroundColor: '$green3 00',
+  },
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
 export const App = () => {
   return <Button>Hello World</Button>
 }
+
+export type ButtonProps = ComponentProps<typeof Button>
