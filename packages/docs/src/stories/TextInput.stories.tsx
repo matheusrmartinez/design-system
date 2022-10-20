3a684147d6d18f6d81169e532c4e643b2aa59e70
Matheus Martinez <matheus.martinez@ctw.bmwgroup.com>
2022-10-20 21:07:43 +0100

    Added TextInput story

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,38 @@
+import { Box, TextInput, TextInputProps, Text } from '@dev-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">E-mail address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
