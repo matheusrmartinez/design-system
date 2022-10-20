9063839fc8a51b75e8371a7aa0c765a35a620eaf
Matheus Martinez <matheus.martinez@ctw.bmwgroup.com>
2022-10-20 21:39:53 +0100

    Added Checkbox Story

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,20 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@dev-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box as="label" css={{ display: 'flex', gap: '$2' }}>
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
